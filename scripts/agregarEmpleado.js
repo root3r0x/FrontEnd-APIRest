7d6a55f04b87636d9d37acfd813a7dcc6a5e29a7
Migue LD <ledesmaduarte@duck.com>
2022-12-20 03:17:50 -0600

    Datetime funcional, formulario mejorado.

---
+++ scripts/agregarEmpleado.js
@@ -0,0 +1,13 @@
+/*
+ * Archivo javascript para empleados.
+ * 
+ */
+
+function dataTimePicker() {
+  console.log("Ingresa tu fecha de nacimiento");
+}
+
+
+window.onload = function () {
+  dataTimePicker();
+}
---
+++ scripts/calendario.js
@@ -0,0 +1,9 @@
+$('.calendario').datepicker({
+  autoclose: true,
+  format: "dd/mm/yyy",
+  todayBtn: "linked",
+  clearBtn: true,
+  language: "es",
+  todayHighlight: true
+});
+
