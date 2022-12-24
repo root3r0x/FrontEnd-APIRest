8604a206683756b8d910928d18a9e7a27596bb2e
Migue LD <ledesmaduarte@duck.com>
2022-12-24 03:23:14 -0600

    Post solucionado, ya agrega usuarios.

--- scripts/agregarEmpleado.js
+++ scripts/agregarEmpleado.js
@@ -41,84 +41,46 @@ function limpiaCampos() {
 
 function envioJSON() {
 
+/**
   nombre.value = "Miguel";
   apellidos.value = "Guadalupe";
-  cedula.value = "90IO90IKIO";
+  cedula.value = "90IO90aasKIO";
   //"edad": parseInt(edad.value), Edad del formulario
   edad.value = 27;
   telefono.value = "8992491860";
-
+**/
   var fech = fecha_nacimiento.value.toString();
   var matriz = fech.split('/'); //["12","14","2022"]
   var fecha = matriz[2] + "-" + matriz[0] + "-" + matriz[1];
   //Datos
   const data = {
-    nombre: nombre.value.toString(),
-    apellidos: apellidos.value.toString(),
-    cedula: cedula.value.toString(),
-    edad: parseInt(edad.value),
-    telefono: telefono.value.toString(),
-    fecha_nacimiento: fecha
+    "nombre": nombre.value.toString(),
+    "apellidos": apellidos.value.toString(),
+    "cedula": cedula.value.toString(),
+    "edad": parseInt(edad.value),
+    "telefono": telefono.value.toString(),
+    "fecha_nacimiento": fecha
   }
-  console.log(data);
-  console.log(data[5]);
-
+  //debugger;
   //fetch('http://localhost:8081/addEmploye.html',{
   fetch('http://localhost:8080/empleados', {
     method: 'POST',
-    mode: 'no-cors',
     body: JSON.stringify(data),
     headers: {
       'Content-Type': 'application/json',
     }
   })
-    .then(resp => resp.text()).then(console.log)
+    //.then(resp => resp.text()).then(console.log)
     .then(res =>
       res.json())
     .then(data =>
-      console.log(data))
+      console.log(data + "<=Esta es la databro"))
     .catch(
       function (error) {
         swal("Error!", 'Hubo un problema con la petición Fetch:' + error.message, "error");
         console.log('Hubo un problema con la petición Fetch:' + error.message);
       })
-}
-
-function lenvioJSON() {
-
-  var fech = fecha_nacimiento.value.toString();
-  var matriz = fech.split('/'); //["12","14","2022"]
-  var fecha = matriz[2] + "-" + matriz[0] + "-" + matriz[1];
-
-  const data = [
-    {
-      "nombre": nombre.value = "Miguel",
-      "apellidos": apellidos.value = "Guadalupe",
-      "cedula": cedula.value = "90IO90IKIO",
-      //"edad": parseInt(edad.value), Edad del formulario
-      "edad": edad.value = 27,
-      "telefono": telefono.value = "8992491860",
-      "fecha_nacimiento": fecha
-    }
-  ]
-  console.log("Datos:", data);
-  //const data = {username: 'example'}
-  fetch("http://localhost:8081/addEmploye.html", {
-    method: 'POST', // or 'PUT'
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  /**
+      /**
     .then(function (data) {
       if (data.ok) {
         return data.text();
@@ -133,8 +95,8 @@ function lenvioJSON() {
     .catch(function (error) {
       console.log(error);
     }); */
-
 }
+
 
 //Botones
 btnAdd.addEventListener('click', function (e) {
@@ -162,10 +124,6 @@ btnCancel.addEventListener('click', function (e) {
           icon: "success",
         });
       }
-      /**
-      else {
-        swal("");
-      }*/
     });
 });
 
