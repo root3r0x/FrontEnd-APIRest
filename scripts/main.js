a02b0de3407661028dc8659884fba18e3dc39aee
Migue LD <ledesmaduarte@duck.com>
2023-01-14 19:21:00 -0600

    [index.html, main.js] Botones editar-eliminar eventListener Click Ok.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 var contenido = document.querySelector('#tblEmpleados');
 
 //Inputs del formulario
-var btnAdd = document.getElementById('btnAgregarEmpleado');
-var btnCancel = document.getElementById('btnCancelar');
-
+//var btnAdd = document.getElementById('btnAgregarEmpleado');
+//var btnCancel = document.getElementById('btnCancelar');
 
 function traer() {
   //fetch('http://127.0.0.1:8080/empleados')
@@ -15,7 +14,7 @@ function traer() {
 }
 
 function tabla(datos) {
-  //console.log(datos)
+  
   tblEmpleados.innerHTML = ''
 
   for (let valor of datos) {
@@ -39,7 +38,7 @@ function tabla(datos) {
             Editar
           </button>
           
-          <button name="${valor.id}" type="button" class="btn btn-danger btn-sm">
+          <button name="${valor.id}" type="submit" class="btn btn-danger btn-sm">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
             </svg>
@@ -51,10 +50,17 @@ function tabla(datos) {
     </tr>
     `
   }
-}
-//function validaciones
-var txtNombre = document.getElementById("")
-function validaForm() {
+
+  //Tabla de empleados.
+  const botones = document.getElementById('tblEmpleados').querySelectorAll('button');
+
+  Array.from(botones).forEach(b =>
+    b.addEventListener("click",function(){
+      console.log(b.className + " Clicked");
+      console.log(b.name + " Clicked");
+    })
+  );
+
 }
 
 window.onload = function () {
